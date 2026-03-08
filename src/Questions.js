4b44dcdf328ccddc0e200de573f8f329d23815b2
Carson Wilkie <cwilkie@colgate.edu>
2026-03-08 14:33:01 -0400

    bug

--- src/Questions.js
+++ src/Questions.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
 import { Analytics } from "@vercel/analytics/react";
