34803f155bcd9f3b7372bd5fafed701028174aab
Carson Wilkie <cwilkie@colgate.edu>
2026-04-03 22:26:33 -0400

    llb style

--- src/Questions.js
+++ src/Questions.js
@@ -585,7 +585,7 @@ function Questions() {
             {/* Get Question / Generate Interview button */}
             {!interviewModeOn ? (
               <>
-                {loadingQuestion ? ( /* change back to loadingQuestion*/
+                {loadingQuestion ? ( /* set to true to always see loading bar (for testing) - change back to loadingQuestion*/
                   <LightsaberLoader percent={streamProgress} />
                 ) : (
                   <button onClick={getQuestion} disabled={loadingAnswer} className="primary-btn">
